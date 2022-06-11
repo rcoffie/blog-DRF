a5f5b98a647a2f9c30a93c6e08f8267f006e4ac1
Robert Coffie <rcoffie22@yahoo.com>
2022-06-11 13:40:45 +0000

    creating author list and detail views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from accounts. views import *
+from accounts. views import AuthorPost, AuthorPostDetail
 
 
 app_name = 'accounts'
 
 urlpatterns = [
-    
+ path('author-post/',AuthorPost.as_view()),
+ path('author-post-detail/<int:pk>',AuthorPostDetail.as_view())
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from posts. models import Post, Comment , Category
+from posts.serializers import PostSerializer, CommentSerializer,CategorySerializer
+from rest_framework import generics
 # Create your views here.
+
+
+class AuthorPost(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class AuthorPostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
