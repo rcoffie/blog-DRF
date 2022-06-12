f4fccd0f0e4fe1ce26ff74009892ca2a9bd71e6c
Robert Coffie <rcoffie22@yahoo.com>
2022-06-12 14:21:24 +0000

    working on login and registration endpoint

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,12 @@ from posts.serializers import PostSerializer, CommentSerializer,CategorySerializ
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from accounts.permissions import AuthUserPost
+from rest_framework.decorators import api_view
+from accounts.serializers import RegistrationSerializer
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
 # Create your views here.
 
 
@@ -17,3 +23,24 @@ class AuthorPostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [AuthUserPost]
+#
+@api_view(['POST',])
+def registration_view(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            account = serializer.save()
+
+            Token.objects.get_or_create(user=account)
+            return Response(serializer.data)
+
+
+@api_view(['POST',])
+def logout_view(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+
+# class RegisterView(generics.ListCreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = RegistrationSerializer
