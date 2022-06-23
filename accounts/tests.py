c898d736a45c1beef959bcf0a802d8c8ef07b701
Robert Coffie <rcoffie22@yahoo.com>
2022-06-23 15:15:48 +0000

    testing register and post-list view

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
-
+# from django.test import TestCase
+from django.urls import reverse, include, path
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase, URLPatternsTestCase
 # Create your tests here.
+
+class RegisterTestCase(APITestCase):
+    def test_registration(self):
+        data = {
+        "username":"username",
+        "email":"email",
+        "password":"password",
+        "password2":"password"
+        }
+        # url = reverse('accounts:register')
+        url = '/accounts/register/'
+        response = self.client.post(url,data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+# from django.test import TestCase
+from django.urls import reverse, include, path
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase, URLPatternsTestCase
 
 # Create your tests here.
+
+class ListPost(APITestCase, URLPatternsTestCase):
+    urlpatterns = [
+    path('', include('posts.urls', ))
+    ]
+
+    def test_list_post(self):
+        url = '/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
